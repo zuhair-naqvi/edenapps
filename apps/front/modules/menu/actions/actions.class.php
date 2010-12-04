8a3496207e1b10989194beefe6eb34814b364e4c
Zuhair Naqvi <zuhairnaqvi@Zuhair-Naqvis-MacBook-Pro.local>
2010-12-05 03:25:10 +1100

    plist rendering

--- apps/front/modules/menu/actions/actions.class.php
+++ apps/front/modules/menu/actions/actions.class.php
@@ -43,7 +43,7 @@ class menuActions extends sfActions
   private function xmlToPlist($xml)
   {
   	$xml = preg_replace("/<([0-9]*)\b[^>]*>/","",$xml);
-  	//$xml = preg_replace("/<[/]([0-9]*)\b[^>]*>/","",$xml);
+  	$xml = preg_replace("/<[/]([0-9]*)\b[^>]*>/","",$xml);
   	return $xml;
   }
   
