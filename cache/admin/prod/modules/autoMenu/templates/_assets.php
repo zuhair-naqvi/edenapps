c9031267154023a46289cef9fa8a2f713b343902
root <root@domU-12-31-38-04-BE-DD.compute-1.internal>
2010-12-05 05:07:47 +0000

    admin theme final

--- cache/admin/prod/modules/autoMenu/templates/_assets.php
+++ cache/admin/prod/modules/autoMenu/templates/_assets.php
@@ -3,7 +3,7 @@
   <?php use_javascript('/sfAdminThemejRollerPlugin/js/jquery.min.js', 'first') ?>
   <?php use_javascript('/sfAdminThemejRollerPlugin/js/jquery-ui.custom.min.js', 'first') ?>
 
-  <?php use_stylesheet('/sfAdminThemejRollerPlugin/css/jquery/redmond/jquery-ui.custom.css') ?>
+  <?php use_stylesheet('/sfAdminThemejRollerPlugin/css/jquery/eggplant/jquery-ui.custom.css') ?>
 
   <?php use_stylesheet('/sfAdminThemejRollerPlugin/css/jroller.css') ?>
 
