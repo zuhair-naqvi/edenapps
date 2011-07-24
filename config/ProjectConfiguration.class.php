f9b6d3ccd6392583e4ab8c226817edebff8135a3
Zuhair Naqvi <zuhair@Zuhair-Naqvis-Mac-mini.local>
2011-07-24 16:22:51 +1000

    symf conf

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -2,7 +2,8 @@
 ini_set('date.timezone', 'Australia/Melbourne');
 ini_set('auto_detect_line_endings',true);
 
-require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once '/usr/share/php/symfony/lib/autoload/sfCoreAutoload.class.php';
+//require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
