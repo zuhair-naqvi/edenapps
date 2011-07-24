80d3244c89186b7074c66253f0390539a5d2db40
Zuhair Naqvi <zuhair@Zuhair-Naqvis-Mac-mini.local>
2011-07-24 19:15:02 +1000

    bookings

---
+++ apps/front/modules/bookings/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * bookings actions.
+ *
+ * @package    edenapps
+ * @subpackage bookings
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class bookingsActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    if($request->isMethod('post'))
+    {
+        $eventData = $request->getParameter('eventdata');
+        mail('hi.zuhair@gmail.com', 'New Booking', $eventData);
+    }
+  }
+}
--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -3,6 +3,7 @@ ini_set('date.timezone', 'Australia/Melbourne');
 ini_set('auto_detect_line_endings',true);
 
 require_once '/usr/share/php/symfony/lib/autoload/sfCoreAutoload.class.php';
+
 //require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
